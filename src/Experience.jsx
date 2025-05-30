9530bf2807bf494f757865d78977c7764f84c6ad
RuiMSMelo <ruimsmelo@gmail.com>
2025-05-30 12:11:46 +0100

    postprocessing and postprocessing/r3f done

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,31 +1,74 @@
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
+import {
+    Bloom,
+    Noise,
+    Glitch,
+    ToneMapping,
+    EffectComposer,
+    Vignette,
+    DepthOfField,
+} from '@react-three/postprocessing'
+import { ToneMappingMode, BlendFunction, GlitchMode } from 'postprocessing'
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+    return (
+        <>
+            <color args={['#ffffff']} attach='background' />
 
-        <Perf position="top-left" />
+            <EffectComposer>
+                {/* <Vignette
+                    offset={0.3}
+                    darkness={0.9}
+                    // blendFunction={BlendFunction.COLOR_BURN}
+                /> */}
+                {/* <Glitch
+                    delay={[1, 3]}
+                    duration={[0.1, 0.5]}
+                    strength={[0.4, 0.8]}
+                    mode={GlitchMode.SPORADIC}
+                /> */}
+                {/* <Noise
+                premultiply
+                blendFunction={BlendFunction.SOFT_LIGHT}
+                /> */}
+                {/* <Bloom luminanceThreshold={1.1} mipmapBlur intensity={2} /> */}
+                {/* <DepthOfField
+                    focusDistance={0.025}
+                    focalLength={0.025}
+                    bokehScale={6}
+                /> */}
 
-        <OrbitControls makeDefault />
+                <ToneMapping mode={ToneMappingMode.ACES_FILMIC} />
+            </EffectComposer>
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
-        <ambientLight intensity={ 1.5 } />
+            <Perf position='top-left' />
 
-        <mesh castShadow position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+            <OrbitControls makeDefault />
 
-        <mesh castShadow position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+            <directionalLight castShadow position={[1, 2, 3]} intensity={4.5} />
+            <ambientLight intensity={1.5} />
 
-        <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
+            <mesh castShadow position-x={-2}>
+                <sphereGeometry />
+                <meshStandardMaterial color='orange' />
+            </mesh>
 
-    </>
+            <mesh castShadow position-x={2} scale={1.5}>
+                <boxGeometry />
+                <meshStandardMaterial color='mediumpurple' />
+                {/* <meshBasicMaterial color={[5, 2, 1]} toneMapped={false} /> */}
+            </mesh>
+
+            <mesh
+                receiveShadow
+                position-y={-1}
+                rotation-x={-Math.PI * 0.5}
+                scale={10}
+            >
+                <planeGeometry />
+                <meshStandardMaterial color='greenyellow' />
+            </mesh>
+        </>
+    )
 }
